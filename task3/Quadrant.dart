649dc34c3052d54ea5657b0d2ca7ec37da3008c9
Abdo220044 <166980473+Abdo220044@users.noreply.github.com>
2024-08-29 19:04:37 +0300

    Update Quadrant.dart

--- task3/Quadrant.dart
+++ task3/Quadrant.dart
@@ -1,4 +1,4 @@
-int findQuadrant(int x, int y) {
+int quadrant(int x, int y) {
   if (x > 0 && y > 0) {
     return 1;
   } else if (x < 0 && y > 0) {
@@ -7,15 +7,7 @@ int findQuadrant(int x, int y) {
     return 3;
   } else if (x > 0 && y < 0) {
     return 4;
-  } else {
-    throw ArgumentError('x and y should be non-zero integers.');
   }
-}
 
-void main() {
-  print(findQuadrant(1, 2)); // Output: 1
-  print(findQuadrant(3, 5)); // Output: 1
-  print(findQuadrant(-10, 100)); // Output: 2
-  print(findQuadrant(-1, -9)); // Output: 3
-  print(findQuadrant(19, -56)); // Output: 4
+  return 0;
 }
