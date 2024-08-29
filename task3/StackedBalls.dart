3cb9ff28489fc5b97bd3794b598033821e3dcb37
Abdo220044 <166980473+Abdo220044@users.noreply.github.com>
2024-08-29 19:07:30 +0300

    Update StackedBalls.dart

--- task3/StackedBalls.dart
+++ task3/StackedBalls.dart
@@ -1,9 +1,6 @@
-double stackHeight(int layers) {
-  const double sqrt3Over2 = 0.86602540378; // Approximation of sqrt(3)/2
-  if (layers == 0) return 0;
-  return 1 + (layers - 1) * sqrt3Over2;
-}
+import 'dart:math';
 
-void main() {
-  print(stackHeight(5));
+double stackHeight2d(int cells) {
+  if (cells == 0) return 0.0;
+  return (cells - 1) * (sqrt(3) / 2) + 1.0;
 }
